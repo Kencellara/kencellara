d8955821700a3c2fd46c0cd001a39fc81dda75f1
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-07-18 18:21:15 +0900

    実績追加

--- app/public/wp-content/themes/habakiri/modules/toppage-main.php
+++ app/public/wp-content/themes/habakiri/modules/toppage-main.php
@@ -448,68 +448,7 @@
 <div class="kencemeshiHistory">
   <div class="kencemeshiHistoryHeader topPageHeader">ケンチェ飯の実績</div>
   <div class="kencemeshiHistoryContents">
-    <div class="year">
-      <h3>2020年</h3>
-      <div class="month">
-        <h4>8月</h4>
-        <ul>
-          <li>「ケンチェ飯」本格始動</li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>9月</h4>
-        <ul>
-          <li>GoogleAdsense広告連携開始</li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>11月</h4>
-        <ul>
-          <li><span style="color: #ff0000;"><strong>月間アクセス数 12703 PV</strong></span></li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>12月</h4>
-        <ul>
-          <li><strong><span style="color: #ff0000;">月間アクセス数  13948 PV</span></strong></li>
-        </ul>
-      </div>
-    </div>
-    <div class="year">
-      <h3>2021年</h3>
-      <div class="month">
-        <h4>1月</h4>
-        <ul>
-          <li><strong><span style="color: #ff0000;">月間アクセス数 18757 PV</span></strong></li>
-          <li><a href="https://miecolle.net/hito/1282" target="_blank" rel="noreferrer">ミエコレ 飲食店ライターとして「ケンチェ飯」掲載  <i class="fas fa-external-link-alt"></i></a></li>
-          <li>にっこう亭 取材依頼 受託</li>
-          <li><a href="https://www.kencellara.com/entry/2021-1-event" target="_blank" rel="noreferrer">江戸橋 富や コラボイベント 開催  <i class="fas fa-external-link-alt"></i></a></li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>2月</h4>
-        <ul>
-          <li><span style="color: #ff0000;"><strong>月間アクセス数 27704 PV</strong></span></li>
-          <li>三重タイムズ「ケンチェ飯」新聞掲載</li>
-          <li><a href="https://otonamie.jp/?page_id=34706" target="_blank" rel="noreferrer">OTONAMIE 地域のWEBメディアとして「ケンチェ飯」掲載  <i class="fas fa-external-link-alt"></i></a></li>
-          <li>CAFE&amp;BAR TRIPLE A 取材依頼 受託</li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>3月</h4>
-        <ul>
-          <li>B&amp;Bフォレスト志摩 取材依頼 受託</li>
-          <li>株式会社えんこね 業務提携</li>
-        </ul>
-      </div>
-      <div class="month">
-        <h4>4月</h4>
-        <ul>
-          <li>ごきげんえびすHANARE 取材依頼 受託</li>
-          <li>麵屋AZITO 取材依頼 受託</li>
-        </ul>
-      </div>
-    </div>
+    <?php get_template_part('modules/history'); ?>
   </div>
   <div class="supportZone">
     <script src="https://codoc.jp/js/cms.js" data-css="red-square" charset="UTF-8" defer></script>
