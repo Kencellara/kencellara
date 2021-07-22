ff89ab4bc59accbec7de2a57053d779719a81951
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-07-22 11:29:59 +0900

    Revert "Revert "機能充実３""

--- app/public/wp-content/themes/habakiri/archive.php
+++ app/public/wp-content/themes/habakiri/archive.php
@@ -15,4 +15,5 @@ add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_templat
 add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('category-style', get_template_directory_uri() . '/css/category.css'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('sp_category-style', get_template_directory_uri() . '/css/sp_category.css'));
+add_action('wp_enqueue_scripts', wp_enqueue_script('sp_ad_article-script', get_theme_file_uri('/js/sp_ad_article.js'), array('jquery')));
 get_template_part( 'home' );
