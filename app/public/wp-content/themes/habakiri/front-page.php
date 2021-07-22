a5676c1fe6d88ad476412f6315128981702e2fd7
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-07-22 11:30:56 +0900

    Revert "Merge pull request #5 from Kencellara/revert-4-ukyo-dev-3"
    This reverts commit e0c12158130eaf884bafa8b94ad5e1996f0e296c, reversing
    changes made to 49b687549ff9ae2829031558e10828445aa26ac2.

--- app/public/wp-content/themes/habakiri/front-page.php
+++ app/public/wp-content/themes/habakiri/front-page.php
@@ -4,8 +4,11 @@
 	add_action('wp_enqueue_scripts', wp_enqueue_style('slick-style', get_template_directory_uri() . '/src/js/slick/slick.css'));
 	add_action('wp_enqueue_scripts', wp_enqueue_style('slick-theme-style', get_template_directory_uri() . '/src/js/slick/slick-theme.css'));
 	add_action('wp_enqueue_scripts', wp_enqueue_script('slick-script', get_theme_file_uri('/src/js/slick/slick.min.js'), array('jquery')));
-	add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css',));
-	add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css',));
+  add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css',));
+  add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css',));
+	add_action('wp_enqueue_scripts', wp_enqueue_script('sp_ad_article-script', get_theme_file_uri('/js/sp_ad_article.js')));
+?>
+<?php
 	function toppage_scripts() {
 		wp_enqueue_script( 'modal-script', get_template_directory_uri() . '/js/modal.js', array('jquery'));
 	}
