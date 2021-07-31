e95f17c4640f70b16115046695079b2864cd28a9
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-07-31 21:14:13 +0900

    404ページ修正

--- app/public/wp-content/themes/habakiri/content-none.php
+++ app/public/wp-content/themes/habakiri/content-none.php
@@ -7,7 +7,7 @@
 		<div class="entry__content">
 			<p>
 				申し訳ございません。<br>
-				お探しの記事は見つかりませんでした。
+				お探しのページは見つかりませんでした。
 			</p>
 			<?php if (is_search()) { ?>
 				<?php get_search_form(); ?>
@@ -15,6 +15,15 @@
 			<p><a href=<?php echo home_url() ?>>
 				<img src="<?php echo $sticker_img_dir; ?>/三重グルメならけんちぇ飯.png" alt="三重グルメならケンチェ飯" />
 			</a></p>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7898839193224300" crossorigin="anonymous"></script>
+			<!-- 横並びダブル広告 -->
+			<ins class="adsbygoogle"
+		       style="display:inline-block;width:345px;height:280px"
+		       data-ad-client="ca-pub-7898839193224300"
+	 	       data-ad-slot="5216822028"></ins>
+			<script>
+			     (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
 		</div>
 	</div>
 </article>
