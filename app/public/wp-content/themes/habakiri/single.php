82bb3e5e5a3f327dd2e0bbf8388028f978c7c4cf
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-08-14 01:18:56 +0900

    バージョン付与

--- app/public/wp-content/themes/habakiri/single.php
+++ app/public/wp-content/themes/habakiri/single.php
@@ -1,5 +1,5 @@
 <?php
-	add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/css/single.css', array('habakiri', 'common-style'), '1.0.7.2'));
+	add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/css/single.css', array('habakiri', 'common-style'), '1.0.9'));
 	add_action('wp_enqueue_scripts', wp_enqueue_style('sp_single-style', get_template_directory_uri() . '/css/sp_single.css'));
 	add_action('wp_enqueue_scripts', wp_enqueue_script('ad_infeed-script', get_theme_file_uri('/js/ad_infeed.js'), array('jquery')));
 	add_action('wp_enqueue_scripts', wp_enqueue_script('modal-script', get_theme_file_uri('/js/modal.js')));
