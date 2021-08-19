cce43c2c0bfa9138bfff282532a50ad07520dd09
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-08-19 15:39:44 +0900

    バージョン付与

--- app/public/wp-content/themes/habakiri/archive.php
+++ app/public/wp-content/themes/habakiri/archive.php
@@ -1,7 +1,7 @@
 <?php
   add_action('wp_enqueue_scripts', wp_enqueue_style('archive-style', get_template_directory_uri() . '/css/archive.css', array('habakiri', 'common-style'), '1.0.5'));
   add_action('wp_enqueue_scripts', wp_enqueue_style('sp_archive-style', get_template_directory_uri() . '/css/sp_archive.css'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css', array(), '1.0.5'));
+  add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css', array(), '1.0.10.1'));
   add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css'));
   add_action('wp_enqueue_scripts', wp_enqueue_style('category-style', get_template_directory_uri() . '/css/category.css', array(), '1.0.10'));
   add_action('wp_enqueue_scripts', wp_enqueue_style('sp_category-style', get_template_directory_uri() . '/css/sp_category.css', array(), '1.0.7'));
