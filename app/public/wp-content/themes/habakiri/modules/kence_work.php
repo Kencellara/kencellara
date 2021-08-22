569f0e609b0ee3ccd46ea099d0ae5f9a92683658
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-08-22 18:23:26 +0900

    ケンチェ飯にできることを大幅改変

--- app/public/wp-content/themes/habakiri/modules/kence_work.php
+++ app/public/wp-content/themes/habakiri/modules/kence_work.php
@@ -8,17 +8,57 @@
   <img src="<?php echo $upload_dir['baseurl']; ?>/toppage/kence_work.jpg" alt="コラボ・取材の依頼" loading="lazy" />
 </div>
 
-<div class="kenceWorkWindowHeader topPageHeader">お問い合わせフォーム</div>
+<div class="kenceWorkWindowHeader topPageHeader">ケンチェ飯にできること</div>
+<div class="kenceWorkWindow kenceWorkContents">
+	<div class="kenceWorkWindowExplain">
+		<p>ケンチェ飯では三重県にある飲食店の取材・執筆を<strong>無料で</strong>承っております。</p>
+		<p>
+			現在、月間サイトアクセス数<span class="p_crimson p_bold">6万回</span>を達成し、その流入者のうち<strong>大半が県内で飲食店を探している</strong>方となっています。<br/>
+			そのため、他のメディアに比べて<strong>効果的かつ直接的に熱いユーザー層に宣伝する</strong>ことができます。<br/>
+			また、一度サイト上でお店を紹介すると<strong>半永久的に宣伝することができる</strong>大きなメリットもあります。<br/>
+			これは雑誌や新聞ではなく、<strong>電子媒体メディアならではの強み</strong>となっています。
+		</p>
+		<p><span class="p_crimson p_bold">三重県の飲食業界を活気づける</span>ために、今後も必死で執筆・改善に取り組んでいきますので是非ご協力をお願いします。</p>
+		<p>詳しくは<strong>下記連絡先</strong>、または<strong>お問い合わせフォーム</strong>からお願いします。<span class="p_crimson p_bold">（注意：名前・連絡先・要件の3点は必ずご記入願います。）</span></p>
+		<!-- modal open -->
+		<div class="kenceWindowModalOpen kenceWorkWindowModalOpen">
+		  <a id="js-modal-open-2" href="" data-target="modal02">ケンチェの窓口</a>
+		</div>
+		<!-- ./modal open -->
+		<!-- modal -->
+		<div id="modal02" class="kenceWindowModal js-modal">
+		  <div class="kenceWindowModalBg js-modal-close"></div>
+		  <div class="kenceWindowModalContent">
+		    <div class="kenceWindowModalContentInner">
+		      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf7z8zl1FGl9_pWK9EOQBTbHDTRJ3hd8yIlKoSIPhl4-LbGfg/viewform?embedded=true" width="639" height="830" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+		      <a class="js-modal-close kenceWindowModalClose" href=""><span>閉じる</span></a>
+		    </div>
+		  </div>
+		</div>
+		<!-- ./modal -->
+		<div class="kenceWorkWindowCaution">
+		  ※Googleフォームが開きます<br/>
+		  ※ページの遷移はございません
+		</div>
+		<div class="kenceAdress">ケンチェ飯の連絡先</div>
+		<ul>
+			<li><a href="https://twitter.com/kencellara_food" target="_blank" rel="noreferrer">Twitter <i class="fas fa-external-link-alt"></i></a></li>
+			<li><a href="https://www.instagram.com/kencellara_food" target="_blank" rel="noreferrer">Instagram <i class="fas fa-external-link-alt"></i></a></li>
+			<li><a href="https://www.facebook.com/kencellarafood" target="_blank" rel="noreferrer">Facebook <i class="fas fa-external-link-alt"></i></a></li>
+			<li><a href="https://lin.ee/kjAkh3g" target="_blank" rel="noreferrer">LINE <i class="fas fa-external-link-alt"></i></a></li>
+			<li>kencellarafood@gmail.com</li>
+		</ul>
+	</div>
+</div>
+
+<!-- <div class="kenceWorkWindowHeader topPageHeader">お問い合わせフォーム</div>
 <div class="kenceWorkWindow kenceWorkContents">
 	<div class="kenceWorkWindowExplain">
 		お問い合わせは下記フォーム（ケンチェの窓口）よりお願い致します。
 	</div>
-	<!-- modal open -->
 	<div class="kenceWindowModalOpen kenceWorkWindowModalOpen">
 	  <a id="js-modal-open-2" href="" data-target="modal02">ケンチェの窓口</a>
 	</div>
-	<!-- ./modal open -->
-	<!-- modal -->
 	<div id="modal02" class="kenceWindowModal js-modal">
 	  <div class="kenceWindowModalBg js-modal-close"></div>
 	  <div class="kenceWindowModalContent">
@@ -28,12 +68,11 @@
 	    </div>
 	  </div>
 	</div>
-	<!-- ./modal -->
 	<div class="kenceWorkWindowCaution">
 	  ※Googleフォームが開きます<br/>
 	  ※ページの遷移はございません
 	</div>
-</div>
+</div> -->
 
 <div class="kencemeshiHistoryHeader topPageHeader">ケンチェ飯の歴史</div>
 <div class="kencemeshiHistoryContents kenceWorkContents">
@@ -81,37 +120,6 @@
     <p>また高齢者の飲食店オーナー様の支援、飲食店の宣伝活動も行っています。</p>
     <p>毎月、取り組んだこと・活動内容は<a href="https://www.kencellara.com/archive/category/%E3%83%96%E3%83%AD%E3%82%B0%E9%81%93">活動の忘備録</a>に記しているので、気になる方はチェックしてみてください。</p>
     <p>このような、ケンチェ飯の目的や活動にご賛同いただける企業・団体・行政などの方々に、ご支援・スポンサー・タイアップのご提案をさせていただいております。<br />何卒、ご理解とご協力を賜りますようお願い申し上げます。</p>
-  </div>
-
-  <div class="whatKencemeshiDoZone">
-    <div class="whatKencemeshiDoHeader">ケンチェ飯にできること</div>
-    <div class="whatKencemeshiDoMain">
-      <p>ケンチェ飯では三重県にある飲食店を<span class="p_crimson p_bold">どのサイトよりも詳しく</span>紹介しています。</p>
-      <p><strong>「お店の新作を紹介したい・・・」</strong></p>
-      <p><strong>「SNSを駆使して集客をしてみたい・・・」</strong></p>
-      <p><strong>「三重県民に自分のお店の存在を知ってもらいたい・・・」</strong></p>
-      <p>ケンチェ飯では、当サイト・SNSを利用して飲食店の抱える悩みを解決できればと考えています。</p>
-      <p>特にお店の紹介記事やトップページに配置した広告を通して、<span class="p_crimson p_bold">お店の認知・理解を読者に促す</span>ことができます。</p>
-      <p>また、当サイトは三重県グルメに特化しているため、<span class="p_crimson p_bold">三重県で飲食店を探す方々に直接宣伝が可能</span>となっています。</p>
-      <p>飲食店の様々な要望にお応えできるかと思うので、お気軽にお問合せください。</p>
-      <ul>
-        <li>取材依頼</li>
-        <li>広告掲載</li>
-        <li>コラボ企画</li>
-        <li>サイト設計開発</li>
-        <li>ご飯のお誘い</li>
-      </ul>
-      <p>上記の依頼だけでなく、メディア出演、寄稿なども承ります。</p>
-      <p>詳しくは下記の連絡先または<span class="p_crimson p_bold"><a href="#kenceWorkWindow">お問い合わせフォーム</a></span>から一報お願いします。</p>
-      <ul>
-        <li><a href="https://twitter.com/kencellara_food" target="_blank" rel="noreferrer">Twitter <i class="fas fa-external-link-alt"></i></a></li>
-        <li><a href="https://www.instagram.com/kencellara_food" target="_blank" rel="noreferrer">Instagram <i class="fas fa-external-link-alt"></i></a></li>
-        <li><a href="https://www.facebook.com/kencellarafood" target="_blank" rel="noreferrer">Facebook <i class="fas fa-external-link-alt"></i></a></li>
-        <li><a href="https://lin.ee/kjAkh3g" target="_blank" rel="noreferrer">LINE <i class="fas fa-external-link-alt"></i></a></li>
-        <li>kencellarafood@gmail.com</li>
-      </ul>
-      <p><strong>名前・連絡先・要件の三点は必ず記載願います。</strong></p>
-    </div>
   </div>
 </div>
 
