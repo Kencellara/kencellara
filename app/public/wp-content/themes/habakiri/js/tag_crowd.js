08e84811f0003b7d4d945715f7970240e85f66db
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2021-08-26 21:24:10 +0900

    タグクラウド開いてるときのボタンの色変更

--- app/public/wp-content/themes/habakiri/js/tag_crowd.js
+++ app/public/wp-content/themes/habakiri/js/tag_crowd.js
@@ -1,8 +1,11 @@
-const toggleCrowd = (idName) => {
-  jQuery(idName).toggleClass('open');
+const toggleCrowd = (kind) => {
+  const btnId = '#js-' + kind + 'Btn';
+  const crowdId = '#' + kind + 'TagCrowd';
+  jQuery(btnId).toggleClass('open');
+  jQuery(crowdId).toggleClass('open');
 }
 
 jQuery(document).ready(function() {
-  jQuery('#js-budgetBtn').on('click', () => toggleCrowd('#budgetTagCrowd'));
-  jQuery('#js-genreBtn').on('click', () => toggleCrowd('#genreTagCrowd'));
+  jQuery('#js-budgetBtn').on('click', () => toggleCrowd('budget'));
+  jQuery('#js-genreBtn').on('click', () => toggleCrowd('genre'));
 });
