178242fd7c9d4ee666b814b57671786c0a3ac4cb
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-01-04 04:11:03 +0900

    歴史欄改良

--- app/public/wp-content/themes/habakiri/modules/history.php
+++ app/public/wp-content/themes/habakiri/modules/history.php
@@ -31,9 +31,9 @@
     <h4>1月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 18,757 PV</span></strong></li>
-      <li><a href="https://miecolle.net/hito/1282" target="_blank" rel="noreferrer">ミエコレ 飲食店ライターとして「ケンチェ飯」掲載<i class="fas fa-external-link-alt"></i></a></li>
+      <li><a href="https://miecolle.net/hito/1282" target="_blank" rel="noreferrer">ミエコレ 飲食店ライターとして「ケンチェ飯」掲載</a><i class="fas fa-external-link-alt"></i></li>
       <li>にっこう亭 様 取材依頼 受託</li>
-      <li><a href="https://www.kencellara.com/entry/2021-1-event" target="_blank" rel="noreferrer">江戸橋 富や 様 コラボイベント 開催<i class="fas fa-external-link-alt"></i></a></li>
+      <li><a href="https://kencellara.com/entry/2021-1-event" target="_blank" rel="noreferrer">江戸橋 富や 様 コラボイベント 開催</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
@@ -41,22 +41,22 @@
     <ul>
       <li><span style="color: #ff0000;"><strong>月間アクセス数 27,704 PV</strong></span></li>
       <li>三重タイムズ「ケンチェ飯」新聞掲載</li>
-      <li><a href="https://otonamie.jp/?page_id=34706" target="_blank" rel="noreferrer">OTONAMIE 地域のWEBメディアとして「ケンチェ飯」掲載<i class="fas fa-external-link-alt"></i></a></li>
-      <li>CAFE&amp;BAR TRIPLE A 様 取材依頼 受託</li>
+      <li><a href="https://otonamie.jp/?page_id=34706" target="_blank" rel="noreferrer">OTONAMIE 地域のWEBメディアとして「ケンチェ飯」掲載</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://kencellara.com/entry/triple-a" target="_blank" rel="noreferrer">CAFE&amp;BAR TRIPLE A 様 取材依頼 受託</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
     <h4>3月</h4>
     <ul>
-      <li>B&amp;Bフォレスト志摩 様 取材依頼 受託</li>
+      <li><a href="https://kencellara.com/entry/bb-forest" target="_blank" rel="noreferrer">B&amp;Bフォレスト志摩 様 取材依頼 受託</a><i class="fas fa-external-link-alt"></i></li>
       <li>株式会社えんこね 様 業務提携</li>
     </ul>
   </div>
   <div class="month">
     <h4>4月</h4>
     <ul>
-      <li>ごきげんえびすHANARE 様 取材依頼 受託</li>
-      <li>麵屋AZITO 様 取材依頼 受託</li>
+      <li><a href="https://kencellara.com/entry/hanare-spring" target="_blank" rel="noreferrer">ごきげんえびすHANARE 様 取材依頼 受託</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://kencellara.com/entry/menya-azito" target="_blank" rel="noreferrer">麵屋AZITO 様 取材依頼 受託</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
@@ -68,16 +68,16 @@
   <div class="month">
     <h4>6月</h4>
     <ul>
-      <li>Instagramフォロワー様2000人突破</li>
+      <li><span style="color:deeppink;">Instagramフォロワー様2000人突破</span></li>
     </ul>
   </div>
   <div class="month">
     <h4>7月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 50,597 PV</span></strong></li>
-      <li>よろこば食堂 様 取材依頼 受託</li>
-      <li>MZボイス ラジオ出演</li>
-      <li><a href="https://store.line.me/stickershop/product/16142065/ja?ref=gnsh_stickerDetail" target="_blank" rel="noreferrer">ケンチェ飯公式LINEスタンプ販売開始<i class="fas fa-external-link-alt"></i></a></li>
+      <li><a href="https://kencellara.com/entry/yorokoba" target="_blank" rel="noreferrer">よろこば食堂 様 取材依頼 受託</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://youtu.be/819EC3MgjRs" target="_blank" rel="noreferrer">MZボイス ラジオ出演</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://store.line.me/stickershop/product/16142065/ja?ref=gnsh_stickerDetail" target="_blank" rel="noreferrer">ケンチェ飯公式LINEスタンプ販売開始</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
@@ -97,34 +97,34 @@
     <h4>9月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 66,102 PV</span></strong></li>
-      <li>よこやま あおい様 コラボ飯</li>
-      <li>三重県最強グルメブロガー プリンセスシンデレラ様 コラボ飯</li>
+      <li><a href="https://kencellara.com/entry/isemon" target="_blank" rel="noreferrer">よこやま あおい様 コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://kencellara.com/entry/soil-salad" target="_blank" rel="noreferrer">三重県最強グルメブロガー プリンセスシンデレラ様 コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
     <h4>10月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 78,547 PV</span></strong></li>
-      <li>もぐもぐ三重様 コラボ飯</li>
+      <li><a href="https://kencellara.com/entry/kannoshita" target="_blank" rel="noreferrer">もぐもぐ三重様 コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
     <h4>11月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 80,417 PV</span></strong></li>
-      <li>Instagramフォロワー4000人突破</li>
-      <li>ベースメン様 コラボ飯</li>
-      <li>竹寿司様 取材依頼 受注</li>
+      <li><span style="color:deeppink;">Instagramフォロワー4000人突破</span></li>
+      <li><a href="https://kencellara.com/entry/gravy-burger" target="_blank" rel="noreferrer">ベースメン様 コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://kencellara.com/entry/take-sushi" target="_blank" rel="noreferrer">竹寿司様 取材依頼 受注</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
   <div class="month">
     <h4>12月</h4>
     <ul>
       <li><strong><span style="color: #ff0000;">月間アクセス数 84,006 PV</span></strong></li>
-      <li>もぐもぐ三重様 第2回コラボ飯</li>
-      <li>チームプリンセス コラボ飯</li>
+      <li><a href="https://kencellara.com/entry/yellow-tail" target="_blank" rel="noreferrer">もぐもぐ三重様 第2回コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
+      <li><a href="https://kencellara.com/entry/atumura" target="_blank" rel="noreferrer">チームプリンセス コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
       <li>chouchou box様 取材依頼 受託</li>
-      <li>石崎様 コラボ飯</li>
+      <li><a href="https://kencellara.com/entry/gyouza-channel" target="_blank" rel="noreferrer">石崎様 コラボ飯</a><i class="fas fa-external-link-alt"></i></li>
     </ul>
   </div>
 </div>
