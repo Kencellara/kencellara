9196304005ab81a76d5796ae7ac510b7d5db831f
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-02-28 00:19:33 +0900

    バージョン更新

--- app/public/wp-content/themes/habakiri/page.php
+++ app/public/wp-content/themes/habakiri/page.php
@@ -1,12 +1,12 @@
 <?php
-add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/css/single.css', array('habakiri', 'common-style')));
+add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/css/single.css', array('habakiri', 'common-style'), '2.0.2'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('sp_single-style', get_template_directory_uri() . '/css/sp_single.css'));
-add_action('wp_enqueue_scripts', wp_enqueue_style('page-style', get_template_directory_uri() .'/css/page.css', array(), '2.0.1'));
-add_action('wp_enqueue_scripts', wp_enqueue_style('sp_page-style', get_template_directory_uri() .'/css/sp_page.css', array(), '2.0'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('page-style', get_template_directory_uri() .'/css/page.css', array(), '2.0.2'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('sp_page-style', get_template_directory_uri() .'/css/sp_page.css', array(), '2.0.2'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('slick-style', get_template_directory_uri() . '/src/js/slick/slick.css'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('slick-theme-style', get_template_directory_uri() . '/src/js/slick/slick-theme.css'));
 add_action('wp_enqueue_scripts', wp_enqueue_script('slick-script', get_theme_file_uri('/src/js/slick/slick.min.js'), array('jquery')));
-add_action('wp_enqueue_scripts', wp_enqueue_style('collabo_coverage-style', get_template_directory_uri() . '/css/collabo_coverage.css', array(), '2.0.1'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('collabo_coverage-style', get_template_directory_uri() . '/css/collabo_coverage.css', array(), '2.0.2'));
 add_action('wp_enqueue_scripts', wp_enqueue_style('sp_collabo_coverage-style', get_template_directory_uri() . '/css/sp_collabo_coverage.css', array(), '2.0'));
 ?>
 <?php get_header(); ?>
