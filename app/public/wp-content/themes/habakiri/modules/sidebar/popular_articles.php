f13cf303583ceab1de5d1522e79a48a61d07d5fc
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-03-06 22:12:15 +0900

    サイドバー人気記事の件数減らす

--- app/public/wp-content/themes/habakiri/modules/sidebar/popular_articles.php
+++ app/public/wp-content/themes/habakiri/modules/sidebar/popular_articles.php
@@ -2,11 +2,12 @@
   <div class="sb_popularHeader sb_header">人気記事</div>
   <?php
   Habakiri_Base_Functions::setPostViews(get_the_ID());
-  query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=5&order=DESC');
+  query_posts('meta_key=post_views_count&orderby=meta_value_num&posts_per_page=3&order=DESC');
   while (have_posts()) : the_post();
     $post_id = get_the_ID();
-    // $post_views_count = get_post_meta($post_id, 'post_views_count', true);
+    $post_views_count = get_post_meta($post_id, 'post_views_count', true);
   ?>
+    <div style='display:none;'><?= $post_views_count ?></div>
     <div class="sb_popularArticle sb_article">
       <div class="sb_popularArticlesThumbnail sb_articleThumbnail">
         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail($post_id, 'thumbnail', array('loading' => 'lazy')); ?></a>
