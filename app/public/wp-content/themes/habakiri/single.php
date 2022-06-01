a11ded705a07e9476246bfbe8d5477b329bade6e
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-06-02 00:40:55 +0900

    Add gallery notation

--- app/public/wp-content/themes/habakiri/single.php
+++ app/public/wp-content/themes/habakiri/single.php
@@ -1,6 +1,6 @@
 <?php
-	add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/css/single.css', array('habakiri', 'common-style'), '2.0.2'));
-	add_action('wp_enqueue_scripts', wp_enqueue_style('sp_single-style', get_template_directory_uri() . '/css/sp_single.css', array(), '1.0.10'));
+	add_action('wp_enqueue_scripts', wp_enqueue_style('single-style', get_template_directory_uri() . '/scss-css/single.css', array('habakiri', 'common-style'), '3.0'));
+	add_action('wp_enqueue_scripts', wp_enqueue_style('sp_single-style', get_template_directory_uri() . '/scss-css/sp_single.css', array(), '3.0'));
 	add_action('wp_enqueue_scripts', wp_enqueue_script('ad_infeed-script', get_theme_file_uri('/js/ad_infeed.js'), array('jquery')));
 	add_action('wp_enqueue_scripts', wp_enqueue_script('modal-script', get_theme_file_uri('/js/modal.js')));
 ?>
