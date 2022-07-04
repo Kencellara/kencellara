b9452bf7f62b1ea5cec87c58a8bf34f3e327be58
r.ukyo <43462697+R-ukyo@users.noreply.github.com>
2022-07-05 08:19:47 +0900

    記事中画像の横幅100%に

--- app/public/wp-content/themes/habakiri/tag.php
+++ app/public/wp-content/themes/habakiri/tag.php
@@ -1,47 +1,47 @@
 <?php
-  add_action('wp_enqueue_scripts', wp_enqueue_style('tag-style', get_template_directory_uri() . '/css/tag.css', array('habakiri', 'common-style'), '1.0.12'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('sp_tag-style', get_template_directory_uri() . '/css/sp_tag.css', array(), '1.0.12'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('archive-style', get_template_directory_uri() . '/css/archive.css', array(), '1.0.12'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('sp_archive-style', get_template_directory_uri() . '/css/sp_archive.css', array(), '1.0.12'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css', array(), '1.0.7'));
-  add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css', array(), '1.0.7'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('tag-style', get_template_directory_uri() . '/css/tag.css', array('habakiri', 'common-style'), '1.0.12'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('sp_tag-style', get_template_directory_uri() . '/css/sp_tag.css', array(), '1.0.12'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('archive-style', get_template_directory_uri() . '/css/archive.css', array(), '1.0.12'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('sp_archive-style', get_template_directory_uri() . '/css/sp_archive.css', array(), '1.0.12'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('new_posts-style', get_template_directory_uri() . '/css/new_posts.css', array(), '1.0.7'));
+add_action('wp_enqueue_scripts', wp_enqueue_style('sp_new_posts-style', get_template_directory_uri() . '/css/sp_new_posts.css', array(), '1.0.7'));
 ?>
 <?php
-  $tag = get_queried_object();
-  $multi_tags_arr = $wp_query->query_vars['tag_slug__and'];
-  $is_multi = !!$multi_tags_arr;
-  if ($is_multi) {
-    foreach ($multi_tags_arr as &$value) {
-      $tags = get_tags(array('slug' => $value));
-      $value = $tags[0]->name;
-    }
-    unset($value);
+$tag = get_queried_object();
+$multi_tags_arr = $wp_query->query_vars['tag_slug__and'];
+$is_multi = !!$multi_tags_arr;
+if ($is_multi) {
+  foreach ($multi_tags_arr as &$value) {
+    $tags = get_tags(array('slug' => $value));
+    $value = $tags[0]->name;
   }
-  $tag_name = $tag->name;
+  unset($value);
+}
+$tag_name = $tag->name;
 ?>
 <?php get_header(); ?>
 
-<?php get_template_part( 'modules/page-header' ); ?>
+<?php get_template_part('modules/page-header'); ?>
 <div class="sub-page-contents">
   <div class="container">
-  	<div class="row">
-  		<div class="col-md-9 tag">
-  			<main id="main" role="main">
+    <div class="row">
+      <div class="col-md-9 tag">
+        <main id="main" role="main">
           <div class="tagSearchZone">
-            <?php if ($is_multi): ?>
+            <?php if ($is_multi) : ?>
               <div class="commonHeader tagSearchHeader multi">『<?php echo implode(" × ", $multi_tags_arr); ?>』のお店</div>
               <div class="entries tag">
-                <?php while ( have_posts() ) : the_post(); ?>
-                  <?php get_template_part( 'content', 'summary' ); ?>
+                <?php while (have_posts()) : the_post(); ?>
+                  <?php get_template_part('content', 'summary'); ?>
                 <?php endwhile; ?>
               </div>
-            <?php else: ?>
+            <?php else : ?>
               <div class="commonHeader tagSearchHeader">『<?php echo $tag_name; ?>』のお店</div>
-              <?php while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part( 'content', 'tag_summary' ); ?>
+              <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('content', 'tag_summary'); ?>
               <?php endwhile; ?>
             <?php endif; ?>
-            <?php get_template_part( 'modules/pagination' ); ?>
+            <?php get_template_part('modules/pagination'); ?>
           </div>
           <div class="toTopBtnZone toSearch">
             <a class="toTopBtn" href=<?php echo home_url('#areaSearch') ?>>地域で検索</a>
@@ -50,12 +50,12 @@
           <div class="toTopBtnZone toTop">
             <a class="toTopBtn" href=<?php echo home_url() ?>>トップへ</a>
           </div>
-  			</main>
-  		</div>
-  		<div class="col-md-3">
-  			<?php get_sidebar(); ?>
-  		</div>
-  	</div>
+        </main>
+      </div>
+      <div class="col-md-3">
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
   </div>
 </div>
 
